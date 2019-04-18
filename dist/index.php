a5ebb3023e64f56d9256c738315cfca3d20887ae
Alexandre Tobia <alexandre.tobia@gmail.com>
2019-04-18 10:57:40 +0200

    Add prod title and meta

--- dist/index.php
+++ dist/index.php
@@ -1,4 +1,15 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sojade - Marie Claire</title>
+</head>
+<body>
 <?php echo file_get_contents("http://www.marieclaire.fr/chunk/top/2615969"); ?>
 <?php echo file_get_contents("./index.html"); ?>
 <?php echo file_get_contents("http://www.marieclaire.fr/chunk/bottom/2615969"); ?>
-
+</body>
+</html>
